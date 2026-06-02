091735bb74fc0dfdcc298d52993e26658cc91b50
kacn1z-del <kacn1z@gmail.com>
2026-06-01 20:44:16 -0600

    Actualizar App.js

--- App.js
+++ App.js
@@ -1,36 +1,18 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import AdminDashboardScreen from './screens/admin/AdminDashboardScreen';
 import CreateOrderScreen from './screens/client/CreateOrderScreen';
-import DashboardScreen from './screens/restaurant/DashboardScreen';
-import TrackingScreen from './screens/delivery/TrackingScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Admin">
+      <Stack.Navigator>
         <Stack.Screen 
-          name="Admin" 
-          component={AdminDashboardScreen}
-          options={{ title: 'Súper Admin' }}
-        />
-        <Stack.Screen 
-          name="Cliente" 
+          name="CrearPedido" 
           component={CreateOrderScreen}
-          options={{ title: 'Crear Pedido' }}
-        />
-        <Stack.Screen 
-          name="Restaurante" 
-          component={DashboardScreen}
-          options={{ title: 'Panel Restaurante' }}
-        />
-        <Stack.Screen 
-          name="Delivery" 
-          component={TrackingScreen}
-          options={{ title: 'Tracking' }}
+          options={{ title: 'Acosta-Food' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
