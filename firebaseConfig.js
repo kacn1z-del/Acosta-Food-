b8521dfe2f18ebb28ca8d023eef575cfe67acc5f
kacn1z-del <kacn1z@gmail.com>
2026-06-07 22:03:26 -0600

    Actualizar firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getDatabase } from "firebase/database";
+// firebaseConfig.js
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBLVHsF0VqPorPkK0auaWUH_4-k-loC6iU",
+  authDomain: "acosta-food.firebaseapp.com",
+  projectId: "acosta-food",
+  storageBucket: "acosta-food.firebasestorage.app",
+  messagingSenderId: "605529235094",
+  appId: "1:605529235094:web:035dff54f04af00654acb7",
+  measurementId: "G-QWEN1MWXXZ"
 };
 
-const app = initializeApp(firebaseConfig);
+// Evita inicializar 2 veces en React Native
+const app = getApps().length === 0? initializeApp(firebaseConfig) : getApps()[0];
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const realtimeDB = getDatabase(app);
+export default app;
